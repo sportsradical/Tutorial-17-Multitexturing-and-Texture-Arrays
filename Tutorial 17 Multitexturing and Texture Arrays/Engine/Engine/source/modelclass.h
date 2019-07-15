73cbddc873c2f9f24170e4a95247ba3f08602c87
sportsradical <peereira@live.com.pt>
2019-07-15 15:57:59 +0100

    Add files via upload

---
+++ Tutorial 17 Multitexturing and Texture Arrays/Engine/Engine/source/modelclass.h
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <fstream>
+#include <DirectXMath.h>
+using namespace DirectX;
+using namespace std;
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		XMFLOAT3 position;
+	    XMFLOAT2 texture;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*, char*, char*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView** GetTextureDifusse1();
+	ID3D11ShaderResourceView** GetTextureDifusse2();
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTextures(ID3D11Device*, ID3D11DeviceContext*,char*, char*);
+	void ReleaseTextures();
+
+	bool LoadModel(char*);
+	void ReleaseModel();
+
+private:
+	ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+	ModelType* m_model;
+	TextureClass* m_Texture;
+	TextureClass* m_Texture1;
+};
+
+#endif
